a3613aa12b9f6f42604960761813aee9f85c16ba
Camilla Campbell <camilla.campbell@helsinki.fi>
2025-11-11 15:39:02 +0200

    create button for setting counter to certain value

--- src/app.py
+++ src/app.py
@@ -17,3 +17,9 @@ def increment():
 def reset():
     cnt.reset()
     return redirect("/")
+
+@app.route("/set/<int:new_value>", methods=["POST"])
+def set_value(new_value):
+    cnt.set_value(new_value)
+    return redirect("/")
+    
